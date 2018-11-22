a4bf4110cdbbb92eacbe8feb80cd5eecf7c24ff9
FilippoGraziano98 <graziano.1761694@studenti.uniroma1.it>
2018-11-22 17:27:48 +0100

    version 1.3 - filippo

--- Client/login/login.js
+++ Client/login/login.js
@@ -7,46 +7,45 @@ $(document).ready(function(){
 	console.log("redirect_from: "+redirect);
 	if(redirect=="registration"){
 		document.getElementById("login_sub_header").innerHTML="Congratulations!<br>\nServer accepted your registration.<br>\nYou are now a user of PetBet!<br>\n<br>";
-		//window.location.href=
+		//now removing redirect_from from the visualized url
+		var current_url = window.location.href;
+		var update_url = current_url.split("?")[0]
+		console.log(update_url)
+		if(update_url.includes("login.html")){
+			//window.location.href=update_url; //this resulted in reloading the page
+			history.pushState(null, null, update_url);
+		}
 	}
 });
 
-function setCookie(name,value,days) {
-	var expires = "";
-	if (days) {
-		var date = new Date();
-			date.setTime(date.getTime() + (days*24*60*60*1000));
-			expires = "; expires=" + date.toUTCString();
-	}
-	document.cookie = name + "=" + (value || "")	+ expires + "; path=/";
-}
-
 function login(){
-		if(validaForm()) { 
-			var url='http://filippo-ubuntu:8000/login'
-			var data={'email':$('#email').val(),'password':$('#password').val()}
-			$.post(url, data, function onsuccess(_response){
-				var response = JSON.parse(_response);
-				console.log(response);
-				if(response.msg=="login_success"){
-					console.log("cookies: "+response.cookies);
-					setCookie("sessionid","abcd",true);
-					document.cookies="session-id="+response.cookies;
-					//document.cookies.sessionid=response.cookies;
-					localStorage.cookies="session-id="+response.cookies;
-					sessionStorage.cookies="session-id="+response.cookies;
-					console.log(localStorage.cookies)
-					//goto home with cookies
-					//window.location.replace("../login/login.html?redirect_from=registration")
-				}
-				else if(response.msg=="login_fail"){
-					//window.alert("Your registration failed!\nTry again.")
-					document.getElementById("login_sub_header").innerHTML="Login failed!<br>\nUser and/or Password wrong!<br>"
-				}
-				else {
-				
-				}
-			});
+	if(validaForm()){
+		var url='http://filippo-ubuntu:8000/login'
+		var data={
+			'user':$('#user').val(),
+			'password':$('#password').val()
+		}
+		$.post(url, data, function onsuccess(_response){
+			var response = JSON.parse(_response);
+			console.log(response);
+			if(response.msg=="login_success"){
+				console.log("cookies: "+response.cookies);
+				setCookie("sessionid","abcd",true);
+				document.cookies="session-id="+response.cookies;
+				//document.cookies.sessionid=response.cookies;
+				localStorage.cookies="session-id="+response.cookies;
+				sessionStorage.cookies="session-id="+response.cookies;
+				console.log(localStorage.cookies)
+				//goto home with cookies
+				//window.location.replace("../login/login.html?redirect_from=registration")
+			} else if(response.msg=="login_failure__credentials_not_valid"){
+				document.getElementById("login_sub_header").innerHTML="Login failed!<br>\nUser and/or Password wrong!<br>"
+			} else if(response.msg=="login_failure__server_not_able_to_understand_data"){
+				document.getElementById("reg_sub_header").innerHTML="Server wasn't able to parse your data!<br>\nCheck the way you are sending your requests<br>"
+			} else {
+				console.log("unrecognized_server_response");
+			}
+		});
+		document.forms["login_form"].reset();
 	}
-	document.forms["login_form"].reset();
 };
--- Client/register/register.js
+++ Client/register/register.js
@@ -1,18 +1,34 @@
 function register(){
-	if (validaAccessForm()) {
+	if(validaAccessForm()){
 		var url='http://filippo-ubuntu:8000/register'
-		var data={'username':$('#username').val(),'password':$('#password').val()}
-		$.post(url, data, function onsuccess(response){
+		var data={
+			'username':$('#username').val(),
+			'email':$('#email').val(),
+			'password':$('#password').val(),
+			'nome':$('#inputUtente').val(),
+			'cognome':$('#inputCognome').val(),
+			'sesso':$('#sesso').val(),
+			'giornoNascita':$('#giorno').val(),
+			'meseNascita':$('#mese').val(),
+			'annoNascita':$('#anno').val(),
+			'cittaNascita':$('#nascita').val(),
+			'cittaResidenza':$('#residenza').val(),
+			'indirizzoResidenza':$('#indirizzo').val(),
+			'cap':$('#cap').val(),
+		}
+		$.post(url, data, function onsuccess(_response){
+				var response = JSON.parse(_response);
 				console.log(response)
-				if(response.includes("Registration success.")){
-					//window.alert("Congratulations!\nServer accepted your registration.\nYou are now a user of PetBet!")
+				if(response.msg=="registration_success"){
 					window.location.replace("../login/login.html?redirect_from=registration")
-				}
-				else{
-					//window.alert("Your registration failed!\nTry again.")
-					document.getElementById("reg_sub_header").innerHTML="Your registration failed!<br>\nTry again.<br>"
+				} else if(response.msg=="registration_failure__username_already_in_use"){
+					document.getElementById("reg_sub_header").innerHTML="Username already in use!<br>\nChange username<br>"
+				} else if(response.msg=="registration_failure__server_not_able_to_understand_data"){
+					document.getElementById("reg_sub_header").innerHTML="Server wasn't able to parse your data!<br>\nCheck the way you are sending your requests<br>"
+				} else {
+					console.log("unrecognized_server_response");
 				}
 			});
+		document.forms["register_form"].reset();
 	}
-	document.forms["register_form"].reset();
 };
